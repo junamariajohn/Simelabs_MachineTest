31a05bd5198fa2c3b88be07735f6217df96ca264
junamariajohn <junamaria98@gmail.com>
2021-08-19 16:22:06 +0530

    Components For Form Elements Created

---
+++ src/component/Form/TextInput.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TextInput = (props) => {
+	return (
+		<input
+			name={props.name}
+			className={`${props.className} form-control`}
+			onChange={props.onChange}
+			onBlur={props.onBlur}
+			defaultValue={props.defaultValue}
+			value={props.value}
+			type={props.type}
+		/>
+	);
+};
+
+export default TextInput;
---
+++ src/component/UI/Card/CardBody.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardBody = (props) => {
+	return <div className={`${props.className} card-body`}>{props.children}</div>;
+};
+
+export default CardBody;
